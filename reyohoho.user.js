4dc19691c256807aec54371df3bfe103f4757221
reyohoho <reyohoho@proton.me>
2024-02-24 23:36:51 +0300

    Dynamic site url

--- reyohoho.user.js
+++ reyohoho.user.js
@@ -2,21 +2,20 @@
 // @name         ReYohoho
 // @namespace    https://github.com/reyohoho/reyohoho-chrome-ff-ext/
 // @homepage     https://github.com/reyohoho/reyohoho-chrome-ff-ext/
-// @version      0.5
+// @version      0.6
 // @description  Open ReYohoho directly from kinopoisk.ru
 // @author       reyohoho
 // @match        *://*.kinopoisk.ru/*
 // @icon         https://github.com/reyohoho/reyohoho-chrome-ff-ext/raw/master/images/icon128.png
-// @grant        none
 // @downloadURL  https://raw.githubusercontent.com/reyohoho/reyohoho-chrome-ff-ext/userscript/reyohoho.user.js
 // @updateURL    https://raw.githubusercontent.com/reyohoho/reyohoho-chrome-ff-ext/userscript/reyohoho.user.js
-// @connect      kinopoisk.ru
 // @grant        GM_getResourceText
 // @grant        GM_addStyle
-// @grant        GM_xmlhttpRequest
+// @grant        GM.xmlHttpRequest
 // @grant        GM_info
 // @resource     IMPORTED_CSS https://raw.githubusercontent.com/reyohoho/reyohoho-chrome-ff-ext/master/player.css
 // @run-at       document-end
+// @connect      reyohoho.space
 // ==/UserScript==
 
 let lastUrl = location.href;
@@ -24,23 +23,67 @@ const playerTailId = 'watch-kinopoisk-player-tail';
 const movieTypes = ['film', 'series'];
 const tailImage = `<svg width="100%" height="100%" viewBox="0 0 128 512" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;"><path id="Banner" d="M128,0L0,0L0,512L64,480L128,512L128,0Z" style="fill:url(#_Linear1);"/><g transform="matrix(6.57572e-17,1.0739,-1.08204,6.62559e-17,327.734,298.698)"><g><path d="M78.752,202.827L99.504,239.449L120.551,275.899L78.752,275.727L36.953,275.899L58.001,239.449L78.752,202.827Z" style="fill:url(#_Linear2);"/></g></g><defs><linearGradient id="_Linear1" x1="0" y1="0" x2="1" y2="0" gradientUnits="userSpaceOnUse" gradientTransform="matrix(128,512,-2048,512,0,0)"><stop offset="0" style="stop-color:rgb(00,00,00);stop-opacity:1"/><stop offset="1" style="stop-color:rgb(00,00,00);stop-opacity:1"/></linearGradient><linearGradient id="_Linear2" x1="0" y1="0" x2="1" y2="0" gradientUnits="userSpaceOnUse" gradientTransform="matrix(96.5309,0,0,97.4299,30.4868,251.542)"><stop offset="0" style="stop-color:rgb(255,255,255);stop-opacity:1"/><stop offset="1" style="stop-color:rgb(255,255,255);stop-opacity:1"/></linearGradient></defs></svg>`;
 
-function mountPlayer(movieId) {
-    window.open("https://reyohoho.github.io/reyohoho/#" + movieId, '_blank');
+function mountPlayer(siteUrl) {
+    window.open(siteUrl, '_blank');
 }
 
 function mountPlayerTail(movieId) {
     const playerTile = document.createElement('div');
     playerTile.id = playerTailId;
     playerTile.innerHTML = tailImage;
-    playerTile.addEventListener('click', () => mountPlayer(movieId));
-    playerTile.addEventListener('mouseover', () => { playerTile.style.top = '0px' });
-    playerTile.addEventListener('mouseout', () => { playerTile.style.top = '-32px' });
+    try {
+        GM.xmlHttpRequest({
+            url: 'https://reyohoho.space:4437/reyohoho_link',
+            synchronous: true,
+            method: 'GET',
+            onload: function(r) {
+                if (r.status == 200) {
+                    playerTile.addEventListener('click', () => mountPlayer(r.responseText + "#" + movieId));
+                    playerTile.addEventListener('mouseover', () => {
+                        playerTile.style.top = '0px'
+                    });
+                    playerTile.addEventListener('mouseout', () => {
+                        playerTile.style.top = '-32px'
+                    });
 
-    setTimeout(() => {
-        playerTile.style.top = '-32px';
-    }, 100);
+                    setTimeout(() => {
+                        playerTile.style.top = '-32px';
+                    }, 100);
 
-    document.body.appendChild(playerTile);
+                    document.body.appendChild(playerTile);
+                } else {
+                    playerTile.addEventListener('click', () => mountPlayer("https://reyohoho.github.io/reyohoho/#" + movieId));
+                    playerTile.addEventListener('mouseover', () => {
+                        playerTile.style.top = '0px'
+                    });
+                    playerTile.addEventListener('mouseout', () => {
+                        playerTile.style.top = '-32px'
+                    });
+
+                    setTimeout(() => {
+                        playerTile.style.top = '-32px';
+                    }, 100);
+
+                    document.body.appendChild(playerTile);
+                }
+
+            }
+        });
+    } catch (e) {
+        playerTile.addEventListener('click', () => mountPlayer("https://reyohoho.github.io/reyohoho/#" + movieId));
+        playerTile.addEventListener('mouseover', () => {
+            playerTile.style.top = '0px'
+        });
+        playerTile.addEventListener('mouseout', () => {
+            playerTile.style.top = '-32px'
+        });
+
+        setTimeout(() => {
+            playerTile.style.top = '-32px';
+        }, 100);
+
+        document.body.appendChild(playerTile);
+    }
 }
 
 function removeElement(elementId) {
@@ -55,9 +98,9 @@ function kinopoiskPageHandler() {
     const movieType = pathname[0];
     const isMovieIdNum = /^\d+$/.test(movieId);
 
-    if (typeof movieId === 'undefined'
-        || typeof movieType === 'undefined'
-        || !isMovieIdNum
+    if (typeof movieId === 'undefined' ||
+        typeof movieType === 'undefined' ||
+        !isMovieIdNum
     ) {
         console.error('Watch kinopoisk wrong movie data');
         removeElement(playerTailId);
@@ -79,7 +122,10 @@ new MutationObserver(() => {
         lastUrl = url;
         kinopoiskPageHandler();
     }
-}).observe(document, { subtree: true, childList: true });
+}).observe(document, {
+    subtree: true,
+    childList: true
+});
 
 window.addEventListener('load', kinopoiskPageHandler)
 
